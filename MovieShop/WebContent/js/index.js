aad595d7508c4298e9342d2eb4c96b4149383e34
Amy Huang <goten02028@gmail.com>
2025-03-09 17:00:00 -0700

    updated home page

--- MovieShop/WebContent/js/index.js
+++ MovieShop/WebContent/js/index.js
@@ -1,20 +1,18 @@
 function handleResult(resultData) {
     let html = ""
-    let genresContainerElement = jQuery("#genres-grid-container");
     for (let i = 0; i < resultData.length; i++) {
-        html += '<p class="center"> <a href="movies-list.html?action=browseGenre&value=' + resultData[i]["gName"] + '">' +
-            resultData[i]["gName"] + '</a></p>';
+        html += '<li><a href="movies-list.html?action=browseGenre&value=' + resultData[i]["gName"] + '">' +
+            resultData[i]["gName"] + '</a></li>';
     }
-    genresContainerElement.append(html);
+    jQuery("#genre-list").append(html);
 
     html = ""
     const titles = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
         'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '*'];
-    let titlesContainerElement = jQuery("#titles-grid-container");
     for (let i = 0; i < titles.length; i++) {
-        html += '<p class="center"> <a href="movies-list.html?action=browseTitle&value=' + titles[i] + '">' + titles[i] + '</a></p>'
+        html += '<li><a href="movies-list.html?action=browseTitle&value=' + titles[i] + '">' + titles[i] + '</a>'
     }
-    titlesContainerElement.append(html);
+    jQuery("#title-list").append(html);
 }
 
 jQuery.ajax({
